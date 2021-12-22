044d98ff09ebc768c0bbbd0acb0a3a88e4d3dc41
Kristian Philip Colville <kristiancolville1@gmail.com>
2021-12-22 21:44:41 +0000

    Clean code and add buttons to GUI

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
-from tkinter import *
-
-from tkinter import messagebox
+"""
+Application, Author: Kristian Colville
+"""
+from tkinter import Button, Tk, messagebox
 
 front = Tk()
-front.geometry('450x450')
+front.geometry('600x450')
 
-def popUp():
-    msg = messagebox.showinfo('Pop up', 'Hi there!')
+def pop_up():
+    """Pops up a seperate window after clicking button"""
+    messagebox.showinfo('Pop up', 'Hi there!')
 
-
-buttonA = Button(front, text = 'Push me', command = popUp)
+buttonA = Button(front, text = 'Push me', command = pop_up)
 buttonA.place(x= 0, y= 0)
 
 front.mainloop()
