790edab3b5e63a7c88fb74014d2fd5bf6f481b99
Kristian Philip Colville <kristiancolville1@gmail.com>
2021-12-22 21:19:48 +0000

    Add button and pop up

--- index.py
+++ index.py
@@ -5,4 +5,11 @@ from tkinter import messagebox
 front = Tk()
 front.geometry('450x450')
 
+def popUp():
+    msg = messagebox.showinfo('Pop up', 'Hi there!')
+
+
+buttonA = Button(front, text = 'Push me', command = popUp)
+buttonA.place(x= 0, y= 0)
+
 front.mainloop()
